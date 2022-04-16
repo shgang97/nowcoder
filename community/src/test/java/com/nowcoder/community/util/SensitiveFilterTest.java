062f17e6657c4bc1ccd4b51fef286dd836568e9e
shgang97 <shgang97@163.com>
2022-04-17 00:18:37 +0800

    realize pulishing post and show details

--- community/src/test/java/com/nowcoder/community/util/SensitiveFilterTest.java
+++ community/src/test/java/com/nowcoder/community/util/SensitiveFilterTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
 
 /**
  * @author: shg
